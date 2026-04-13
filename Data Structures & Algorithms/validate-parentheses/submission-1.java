b6c0714c1e4356c2818524633682645cb8da9994
Khabou Omar <omarkhabou46@gmail.com>
2026-04-13 21:22:18 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }else {
+                if (stack.isEmpty()) return false;
+                else {
+                    char a = stack.pop();
+                    if ((a == '(' && c != ')') || (a == '{' && c != '}') || (a == '[' && c != ']')) {
+                       return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
