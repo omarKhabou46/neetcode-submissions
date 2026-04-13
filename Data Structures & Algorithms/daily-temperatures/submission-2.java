77775cfb810340bf902204eb423e288370805147
Khabou Omar <omarkhabou46@gmail.com>
2026-04-13 23:14:07 +0100

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // int[] array = new int[temperatures.length]; 
+        // for (int i = 0; i < temperatures.length; i++) {
+        //     for (int j = i + 1; j < temperatures.length; j++) {
+        //         if (temperatures[j] > temperatures[i]) {
+        //             array[i] = j - i;
+        //             break;
+        //         }
+        //     }
+        // }
+        // array[temperatures.length - 1] = 0;
+        // return array; 
+        Stack<Integer> stack = new Stack<>();
+        int[] array = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+        
+        while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int j = stack.pop();
+                array[j] = i - j;
+        }
+        stack.push(i);
+        }
+
+        // while (!stack.isEmpty()) {
+        //     array[stack.pop()] = 0;
+        // }
+
+        return array;
+    }
+}
